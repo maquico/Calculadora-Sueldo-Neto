2d9bc3ba955c10956b13e0241a458eab71d05c7d
maquico <100245559+maquico@users.noreply.github.com>
2022-05-11 22:41:08 -0400

    Add files via upload

---
+++ CalculoImpuestos/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CalculoImpuestos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SueldoBruto = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AFP = new System.Windows.Forms.TextBox();
+            this.ISR = new System.Windows.Forms.TextBox();
+            this.SFS = new System.Windows.Forms.TextBox();
+            this.SueldoNeto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SueldoBruto
+            // 
+            this.SueldoBruto.Location = new System.Drawing.Point(277, 49);
+            this.SueldoBruto.Name = "SueldoBruto";
+            this.SueldoBruto.Size = new System.Drawing.Size(218, 20);
+            this.SueldoBruto.TabIndex = 0;
+            this.SueldoBruto.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calcular\r\n\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AFP
+            // 
+            this.AFP.Location = new System.Drawing.Point(277, 103);
+            this.AFP.Name = "AFP";
+            this.AFP.Size = new System.Drawing.Size(218, 20);
+            this.AFP.TabIndex = 2;
+            // 
+            // ISR
+            // 
+            this.ISR.Location = new System.Drawing.Point(277, 205);
+            this.ISR.Name = "ISR";
+            this.ISR.Size = new System.Drawing.Size(218, 20);
+            this.ISR.TabIndex = 3;
+            // 
+            // SFS
+            // 
+            this.SFS.Location = new System.Drawing.Point(277, 150);
+            this.SFS.Name = "SFS";
+            this.SFS.Size = new System.Drawing.Size(218, 20);
+            this.SFS.TabIndex = 4;
+            // 
+            // SueldoNeto
+            // 
+            this.SueldoNeto.Location = new System.Drawing.Point(277, 274);
+            this.SueldoNeto.Name = "SueldoNeto";
+            this.SueldoNeto.Size = new System.Drawing.Size(218, 20);
+            this.SueldoNeto.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ingrese su sueldo bruto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(235, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "AFP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(235, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "SFS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(235, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "ISR";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(158, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Su sueldo neto es:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1064, 354);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SueldoNeto);
+            this.Controls.Add(this.SFS);
+            this.Controls.Add(this.ISR);
+            this.Controls.Add(this.AFP);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SueldoBruto);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SueldoBruto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox AFP;
+        private System.Windows.Forms.TextBox ISR;
+        private System.Windows.Forms.TextBox SFS;
+        private System.Windows.Forms.TextBox SueldoNeto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CalculoImpuestos/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoImpuestos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalcularNeto();
+
+        }
+
+        private void CalcularNeto()
+        {
+            // Isr 9%, AFP 7%, SFS 10%
+            double suelBruto, afp, sfs, isr, suelNeto;
+            suelBruto = double.Parse(SueldoBruto.Text);
+
+            afp = suelBruto * 0.07;
+            sfs = suelBruto * 0.10;
+            isr = suelBruto * 0.09;
+
+            suelNeto = suelBruto - afp - sfs - isr;
+
+            AFP.Text= afp.ToString();
+            SFS.Text= sfs.ToString();
+            ISR.Text= isr.ToString();
+            SueldoNeto.Text = suelNeto.ToString();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
